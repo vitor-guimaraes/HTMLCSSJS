3a9218ed1c3fd54a5ee50e88f5648dc574b85060
Vitor-Guimaraes <guimvitor@gmail.com>
2023-08-02 20:26:42 -0300

    Função criar projeto implementada

--- scripts/project-create-edit.js
+++ scripts/project-create-edit.js
@@ -11,3 +11,27 @@ window.onload = function(){
         document.querySelector('#action-button').innerText = 'Salvar';
     }
 }
+
+function cadastrar() {
+    // Inicia a massa de dados (payload)
+    let payload = {
+        title: document.getElementsByName("title"),
+        totalCost: document.querySelector("#totalCost").value,
+        description: document.querySelector("#description").value,
+        idClient: "1"
+    }
+
+    // Enviar para API
+    fetch("https://64c99758b2980cec85c25154.mockapi.io/api/projects", {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        alert('Cadastrado com sucesso!');
+    })
+
+}
